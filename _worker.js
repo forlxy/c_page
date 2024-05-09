ecf1d99ec72c1c802fde20a35cea25c37e372e2f
SamZ <kasaluoqi@gmail.com>
2024-05-09 18:15:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "chat.openai.com/backend-api";
+    url.host = "api.angelxf.cf/api";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
